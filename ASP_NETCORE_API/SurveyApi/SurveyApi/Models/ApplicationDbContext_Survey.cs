7f8cdc2853d6846346250d840ce6c567d7f9d460
dixneufhunter <94339479+dixneufhunter@users.noreply.github.com>
2024-07-06 20:43:30 +0700

    Update Mini Project

--- ASP_NETCORE_API/SurveyApi/SurveyApi/Models/ApplicationDbContext_Survey.cs
+++ ASP_NETCORE_API/SurveyApi/SurveyApi/Models/ApplicationDbContext_Survey.cs
@@ -17,26 +17,6 @@ namespace SurveyApi.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Customer>()
-            //    .HasKey(c => c.customer_id); 
-
-
-            //modelBuilder.Entity<Food>()
-            //  .HasKey(f => f.food_id);
-
-
-            //modelBuilder.Entity<Transaction>()
-            //    .HasKey(t => new { t.transaction_id });
-
-            //modelBuilder.Entity<Transaction>()
-            //    .HasOne(c => c.Customer)
-            //    .WithMany(t => t.Transactions)
-            //    .HasForeignKey(tr => tr.customer_id);
-
-            //modelBuilder.Entity<Transaction>()
-            //    .HasOne(f => f.Food)
-            //    .WithMany(t => t.Transactions)
-            //    .HasForeignKey(tr => tr.food_id);
 
             modelBuilder.Entity<Lembaga_survey>()
                 .HasKey(c => c.ID_LEMBAGA);
